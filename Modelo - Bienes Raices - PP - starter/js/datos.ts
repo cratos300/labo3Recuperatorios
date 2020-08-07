623e8a2531ad42f087f844036b75b19ec7b52235
hernan <hernan-vilar@hotmail.com>
2020-08-07 20:21:29 -0300

    FuncionConChart

--- Modelo - Bienes Raices - PP - starter/js/datos.ts
+++ Modelo - Bienes Raices - PP - starter/js/datos.ts
@@ -37,3 +37,50 @@ export function CargarSelector()
         sltransaccion.appendChild(option);
     }
 }
+export function GuardarBaseLocalStorage($datos)
+{
+    localStorage.setItem("datos", $datos);
+}
+export function TraerBaseLocalStorage()
+{
+    let dates = localStorage.getItem("datos");
+    return JSON.parse(dates);
+    
+}
+export function TraerIDuLTIMO()
+{
+    let resultado;
+    let dates = localStorage.getItem("datos");
+    let dati = JSON.parse(dates);
+    for(let i = 0; i<dati.length;i++)
+    {
+         resultado = dati[i].id;
+    }
+    return resultado;
+}
+export function alta(nuevoAnuncio) 
+{ 
+    let contador1 = 0;
+    let det = TraerBaseLocalStorage()
+   if(det == null || det == "")
+   {
+       let datiii = Array();
+    let siguiente = 1;
+    nuevoAnuncio.id = String(siguiente);
+    datiii.push(nuevoAnuncio);
+    GuardarBaseLocalStorage(JSON.stringify(datiii));
+    //Listar2();
+    contador1 = contador1 +1;
+   }
+   else 
+   {
+    let ultimo = TraerIDuLTIMO();
+    let siguiente = Number(ultimo) +1;
+    nuevoAnuncio.id = String(siguiente);
+    let datiii = TraerBaseLocalStorage();
+    datiii.push(nuevoAnuncio);
+    GuardarBaseLocalStorage(JSON.stringify(datiii));
+    //Listar2();
+   }
+}
+
